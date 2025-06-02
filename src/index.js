c1f6ed679877be32d273112a1b983ea6d0729f68
jkvalani001 <jkvalani001@gmail.com>
2025-06-02 21:16:01 +0530

    utils files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ dotenv.config({
     path:'./env'
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err)=>{
+    console.log("Error connecting to the database:", err);
+    process.exit(1); // Exit process with failure
+});
 
 
 
